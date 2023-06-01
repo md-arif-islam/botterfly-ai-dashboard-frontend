2a3ed34d9d1fe653b5c26ea520c58f5a31df6668
md-arif-islam <arifislamarif344@gmail.com>
2023-06-01 23:56:23 +0600

    update 4 files and copy 1 file

--- app.js
+++ app.js
@@ -1,40 +1,91 @@
-const shrink_btn = document.querySelector(".shrink-btn");
-const sidebar_links = document.querySelectorAll(".sidebar-links a");
-const active_tab = document.querySelector(".active-tab");
-const logo = document.querySelector(".logo");
-const shortcuts = document.querySelector(".sidebar-links h4");
-const tooltip_elements = document.querySelectorAll(".tooltip-element");
+$(document).ready(function () {
+  const shrink_btn = $(".shrink-btn");
+  const sidebar_links = $(".sidebar-links a");
+  const active_tab = $(".active-tab");
+  const logo = $("#logo");
+  const shortcuts = $(".sidebar-links h4");
+  const tooltip_elements = $(".tooltip-element");
+  const toggle_btn_open = $(".toggle-btn-open");
+  const toggle_btn_close = $(".toggle-btn-close");
 
-let r_art = document.querySelector(".r-art");
-let utm = document.querySelector(".utm");
-let slider = document.querySelector(".slider");
-let formSection = document.querySelector(".form-section");
+  let r_art = $(".r-art");
+  let utm = $(".utm");
+  let slider = $(".slider");
+  let formSection = $(".form-section");
 
-r_art.addEventListener("click", () => {
-  slider.classList.add("moveslider");
-  formSection.classList.add("form-section-move");
-});
+  r_art.on("click", function () {
+    slider.addClass("moveslider");
+    formSection.addClass("form-section-move");
+  });
 
-utm.addEventListener("click", () => {
-  slider.classList.remove("moveslider");
-  formSection.classList.remove("form-section-move");
-});
+  toggle_btn_open.on("click", function () {
+    $("body").addClass("mobile");
+  });
 
-let activeIndex;
+  toggle_btn_close.on("click", function () {
+    $("body").removeClass("mobile");
+  });
 
-shrink_btn.addEventListener("click", () => {
-  document.body.classList.toggle("shrink");
-  setTimeout(moveActiveTab, 400);
+  utm.on("click", function () {
+    slider.removeClass("moveslider");
+    formSection.removeClass("form-section-move");
+  });
 
-  shrink_btn.classList.add("hovered");
-  logo.style.display == "none";
-  setTimeout(() => {
-    shrink_btn.classList.remove("hovered");
-  }, 500);
-});
+  let activeIndex;
 
-// Using JavaScript
-const link = document.querySelector("a");
-link.addEventListener("click", () => {
-  link.classList.toggle("active");
+  shrink_btn.on("click", function () {
+    $("#logo").toggleClass("logo-hide");
+    $("body").toggleClass("shrink");
+    setTimeout(moveActiveTab, 400);
+
+    shrink_btn.addClass("hovered");
+
+    setTimeout(function () {
+      shrink_btn.removeClass("hovered");
+    }, 500);
+  });
+
+  // Using jQuery
+  $("a").on("click", function () {
+    $(this).toggleClass("active");
+  });
+
+  $(".upload-label").on("dragover", function (event) {
+    event.preventDefault();
+    $(this).addClass("drag-over");
+  });
+
+  $(".upload-label").on("dragleave", function (event) {
+    event.preventDefault();
+    $(this).removeClass("drag-over");
+  });
+
+  $(".upload-label").on("drop", function (event) {
+    event.preventDefault();
+    $(this).removeClass("drag-over");
+
+    var file = event.originalEvent.dataTransfer.files[0];
+    displayPreview(file);
+  });
+
+  $("#image-upload").on("change", function (event) {
+    var file = event.target.files[0];
+    displayPreview(file);
+  });
+
+  function displayPreview(file) {
+    var reader = new FileReader();
+
+    reader.onload = function (e) {
+      var previewImage = $("#preview-image");
+      previewImage.attr("src", e.target.result);
+      previewImage.css("display", "block");
+
+      var uploadedText = $("#uploaded-text");
+      uploadedText.text(file.name);
+      uploadedText.addClass("uploaded");
+    };
+
+    reader.readAsDataURL(file);
+  }
 });
